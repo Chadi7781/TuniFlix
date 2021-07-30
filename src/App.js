a66282e88aaf241aafe08e4a4cbf8f3f07ebc963
Shady7781 <troudishedy6@gmail.com>
2021-07-30 18:14:04 +0200

    zoom in items

--- src/App.js
+++ src/App.js
@@ -8,8 +8,17 @@ function App() {
   return (
     <div className="App">
       <h1>Hey Let's build Tuniflix</h1>
-      <Row title="TUNIFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}></Row>
+      <Row title="TUNIFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}
+      isLargeRow
+      ></Row>
       <Row title="Trending originals"fetchUrl={requests.fetchTrending}></Row>
+      <Row title="Top Rated"fetchUrl={requests.fetchTopRated}></Row>
+      <Row title="Action Movies"fetchUrl={requests.fetchActionMovies}></Row>
+      <Row title="Comedy Movies"fetchUrl={requests.fetchComedyMovies}></Row>
+      <Row title="Horror Movies"fetchUrl={requests.fetchHorrorMovies}></Row>
+      <Row title=" Romance Movies"fetchUrl={requests.fetchRomanceMovies}></Row>
+      <Row title=" Documentaries"fetchUrl={requests.fetchDocumentaries}></Row>
+
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from './axios';
-function Row({title, fetchUrl}) {
+import './Row.css'
+function Row({title, fetchUrl, isLargeRow}) {
 
     const base_url = "https://image.tmdb.org/t/p/original"
 
@@ -28,12 +29,13 @@ function Row({title, fetchUrl}) {
             <div className="row__posters">
                 {movies.map(movie =>{
                     return <img 
-                    className="row__poster"
-                    src={`${base_url}${movie.poster_path}`
+                    key={movie.id}
+                    className={`row__poster ${isLargeRow &&"row__posterLarge"}`}
+                    src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`
                 } alt={movie.name}/>
             })}
                 
-                
+      
 
             </div>
             {/*  Container */}
